866c13dfd69bf6e1dc2009e6548fdf7c17bc5fc5
Lwx1122 <947386349@qq.com>
2018-10-17 15:11:06 +0800

    前台登陆

---
+++ src/com/aaa/Inter/FrontLoginInter.java
@@ -0,0 +1,38 @@
+package com.aaa.Inter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 
+ * @李文霞 Administrator
+ *2018年10月17日
+ */
+public class FrontLoginInter implements HandlerInterceptor{
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2) throws Exception {
+		
+		return false;
+	}
+	
+
+}
